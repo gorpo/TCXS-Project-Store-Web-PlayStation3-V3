71c87e0a2847a329e59a9faf73942226ce8f88ed
gorpoorko <68359752+gorpoorko@users.noreply.github.com>
2020-09-03 02:49:27 -0300

    Add files via upload

---
+++ admin/tools/upload_watermark_resize/upload.php
@@ -0,0 +1,111 @@
+<?php 
+// Path configuration 
+$targetDir = ""; 
+$watermarkImagePath = 'watermark.png'; 
+ 
+
+
+
+function resize_image($file, $w, $h, $crop=FALSE) {
+    list($width, $height) = getimagesize($file);
+    $r = $width / $height;
+    if ($crop) {
+        if ($width > $height) {
+            $width = ceil($width-($width*abs($r-$w/$h)));
+        } else {
+            $height = ceil($height-($height*abs($r-$w/$h)));
+        }
+        $newwidth = $w;
+        $newheight = $h;
+    } else {
+        if ($w/$h > $r) {
+            $newwidth = $h*$r;
+            $newheight = $h;
+        } else {
+            $newheight = $w/$r;
+            $newwidth = $w;
+        }
+    }
+    $src = imagecreatefromjpeg($file);
+    $dst = imagecreatetruecolor($newwidth, $newheight);
+    imagecopyresampled($dst, $src, 0, 0, 0, 0, $newwidth, $newheight, $width, $height);
+
+    return $dst;
+}
+
+
+
+
+$statusMsg = ''; 
+if(isset($_POST["submit"])){ 
+    if(!empty($_FILES["file"]["name"])){ 
+        // File upload path 
+        $fileName = basename($_FILES["file"]["name"]); 
+        $targetFilePath = $targetDir . $fileName; 
+        $fileType = pathinfo($targetFilePath,PATHINFO_EXTENSION); 
+         
+        // Allow certain file formats 
+        $allowTypes = array('jpg','png','jpeg'); 
+        if(in_array($fileType, $allowTypes)){ 
+            // Upload file to the server 
+            if(move_uploaded_file($_FILES["file"]["tmp_name"], $targetFilePath)){ 
+                // Load the stamp and the photo to apply the watermark to 
+                //echo $targetFilePath;
+                 $im = resize_image($targetFilePath, 200, 200); 
+                
+                $watermarkImg = imagecreatefrompng($watermarkImagePath); 
+                switch($fileType){ 
+                    case 'jpg': 
+                        $im = imagecreatefromjpeg($targetFilePath); 
+                        break; 
+                    case 'jpeg': 
+                        $im = imagecreatefromjpeg($targetFilePath); 
+                        break; 
+                    case 'png': 
+                        $im = imagecreatefrompng($targetFilePath); 
+                        break; 
+                    default: 
+                        $im = imagecreatefromjpeg($targetFilePath); 
+                } 
+                 
+
+
+                // Set the margins for the watermark 
+                $marge_right = 00; 
+                $marge_bottom = 00; 
+                 
+                // Get the height/width of the watermark image 
+                $sx = imagesx($watermarkImg); 
+                $sy = imagesy($watermarkImg); 
+                 
+                // Copy the watermark image onto our photo using the margin offsets and  
+                // the photo width to calculate the positioning of the watermark. 
+
+                //redimensiona a imagem 
+                $im = imagescale($im,250,239);
+                imagecopy($im, $watermarkImg, imagesx($im) - $sx - $marge_right, imagesy($im) - $sy - $marge_bottom, 0, 0, imagesx($watermarkImg), imagesy($watermarkImg)); 
+                 
+                // Save image and free memory 
+                imagepng($im, $targetFilePath); 
+                imagedestroy($im); 
+     
+                if(file_exists($targetFilePath)){ 
+                    $statusMsg = "The image with watermark has been uploaded successfully."; 
+                }else{ 
+                    $statusMsg = "Image upload failed, please try again."; 
+                }  
+            }else{ 
+                $statusMsg = "Sorry, there was an error uploading your file."; 
+            } 
+        }else{ 
+            $statusMsg = 'Sorry, only JPG, JPEG, and PNG files are allowed to upload.'; 
+        } 
+    }else{ 
+        $statusMsg = 'Please select a file to upload.'; 
+    } 
+} 
+ 
+// Display status message 
+//echo $statusMsg;
+echo $fileName;
+echo "<br><br><img src='$targetFilePath'>";
