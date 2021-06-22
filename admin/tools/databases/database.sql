cab900d2c8ee71d3929d35c5606fe6496a140ece
gorpoorko <68359752+gorpoorko@users.noreply.github.com>
2021-06-22 15:58:45 -0300

    Update database.sql

--- admin/tools/databases/database.sql
+++ admin/tools/databases/database.sql
@@ -29,14 +29,7 @@ CREATE TABLE `playstation_users` (
 --
 
 INSERT INTO `playstation_users` (`id`, `usuario`, `senha`, `nome`, `cadastro`,`nivel`) VALUES
-(0, 'marcinho', 'marcinho', 'Marcinho', '2500-01-11 19:42:12','user'),
-(1, 'gorpo', 'gorpo', 'Gorpo', '2500-01-11 19:42:12','admin'),
-(2, 'gorp', 'corp', 'Gorpo', '2500-01-11 19:42:12','admin'),
-(3, 'gorpo', 'corpo', 'Gorpo', '2500-01-11 19:42:12','admin'),
-(4,'mitsuki', 'tcxsproject2020web', 'Mitsuki', '2500-09-07 15:06:19','admin'),
-(5,'mste3dz', 'tcxsproject2020web', 'MsTe3dz', '2500-09-07 15:06:25','admin'),
-(6,'mit', 'mit', 'Mitsuki', '2500-09-07 15:06:25','admin'),
-(7,'ted', 'ted', 'MsTe3dz', '2500-09-07 15:06:25','admin');
+(0, 'marcinho', 'marcinho', 'Marcinho', '2500-01-11 19:42:12','user');
 
 
 --
